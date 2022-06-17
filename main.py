45bacc49b149a6d7a4900139fe338b45296b09ec
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2022-06-17 08:54:48 +0200

    Add simple main file

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+VERSION = "0.1"
+SYSTEM_NAME = "d4k CT Microservice"
+
+app = FastAPI(
+  title = SYSTEM_NAME,
+  description = "A microservice to handle CT in a Neo4j database.",
+  version = VERSION
+)
+
+@app.get("/")
+async def read_root():
+  return { 'system_name': SYSTEM_NAME, 'version': VERSION }
