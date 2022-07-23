1214f591a895bea09d3c64e2e8ec996cfef47dce
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2022-07-23 12:18:45 +0200

    Add further tests

--- tests/test_service_environment.py
+++ tests/test_service_environment.py
@@ -1,5 +1,29 @@
 import pytest
+import os
 from service_environment import ServiceEnvironment
 
-def test_environment():
+def test_environment_set():
   assert ServiceEnvironment().environment() == "test"
+
+def test_environment_invalid():
+  preserve = os.environ['PYTHON_ENVIRONMENT']
+  os.environ['PYTHON_ENVIRONMENT'] = "X"
+  assert ServiceEnvironment().environment() == "X"
+  os.environ['PYTHON_ENVIRONMENT'] = preserve
+
+def test_environment_not_set():
+  preserve_all = os.environ
+  preserve = preserve_all.pop('PYTHON_ENVIRONMENT')
+  assert ServiceEnvironment().environment() == "development"
+  preserve_all['PYTHON_ENVIRONMENT'] = preserve
+  os.environ = preserve_all
+  print(os.environ)
+
+def test_environment_get():
+  preserve = os.environ
+  os.environ['TEST_SOMETHING1'] = "X"
+  assert ServiceEnvironment().get('SOMETHING1') == "X"
+  os.environ = preserve
+
+def test_environment_missing():
+  assert ServiceEnvironment().get('SOMETHING2') == ""
